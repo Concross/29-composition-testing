a40fb839d6a52a72d4641d603714fcbfe946890a
Concross <cacrossley22@gmail.com>
2018-10-17 15:47:53 -0700

    implement add/remove note methods to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,6 +7,23 @@ class Dashboard extends React.Component {
     super(props);
 
     this.state = { notes: [] };
+
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+  }
+
+  addNote(note) {
+    this.state.notes.push(note);
+
+    this.setState({ notes: this.state.notes });
+  }
+
+  removeNote(note) {
+    let notes = this.state.notes;
+
+    notes.splice(notes.indexOf(note), 1);
+
+    this.setState({ notes });
   }
 
   render() {
