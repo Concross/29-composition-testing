0f154720ab889a1ec9cc93873a5b4896db8170fb
Concross <cacrossley22@gmail.com>
2018-10-17 15:55:33 -0700

    create note model

---
+++ src/models/note.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import uuid from 'uuid/v4';
+
+export default class Note {
+  constructor(title, content, isEditing, isCompleted) {
+    this.id = uuid();
+    this.title = title || '';
+    this.content = content || '';
+    this.isEditing = isEditing || false;
+    this.isCompleted = isCompleted || false;
+  }
+}
