5ddb58b27989b2256e7d1095a544df8d1243c396
Concross <cacrossley22@gmail.com>
2018-10-19 14:01:46 -0700

    update form fields prepopulate with note state

--- src/components/note-update-form/index.js
+++ src/components/note-update-form/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Note from '../../models/note';
 
 export default class NoteUpdateForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: this.props.note.title,
+      content: this.props.note.content,
+    };
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    this.setState({ [name]: e.target.value });
+  }
+
   render() {
     return (
-      <p>Hello NoteUpdateForm!</p>
+      <form id="update-note-form" onSubmit={this.onComplete}>
+        <label htmlFor="title">
+          Title:
+          <input type="text" name="title" value={this.state.title} placeholder="Buy milk" onChange={this.handleChange} autoComplete="off" />
+        </label>
+        <label htmlFor="content">
+          Content:
+          <input type="text" name="content" value={this.state.content} placeholder="Details..." onChange={this.handleChange} autoComplete="off" />
+        </label>
+        <button type="submit">Add item</button>
+      </form>
     );
   }
 }
